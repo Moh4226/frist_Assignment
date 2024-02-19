c77ff38a17298abd3c6548c184465e7ace40404d
Moh4226 <ee301da@gmail.com>
2024-02-19 15:58:33 +0300

    Assignment 19/2/2024
    add Singletons in class score

--- Assets/score.cs
+++ Assets/score.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
 public class score : MonoBehaviour
 {
+    public static score Instance;
     [SerializeField] private static TextMeshProUGUI m_Text;
     [SerializeField] private static int m_Score = 0;
     // Start is called before the first frame update
     void Start()
     {
+        Instance=this;
         m_Text = GameObject.Find("scorekill").GetComponent<TextMeshProUGUI>();
+       
     }
 
     // Update is called once per frame
@@ -20,7 +24,7 @@ public class score : MonoBehaviour
         
     }
 
-    public static void writescore() {
+    public void writescore() {
         m_Score++;
         m_Text.text = m_Score.ToString();
     
--- Assets/spherecontrollmove.cs
+++ Assets/spherecontrollmove.cs
@@ -35,7 +35,7 @@ public class spherecontrollmove : MonoBehaviour
            
             Destroy(collision.gameObject);
             Destroy(gameObject);
-            score.writescore();
+            score.Instance.writescore();
 
 
         }
